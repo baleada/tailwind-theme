8636222b17d7c175964a410341b7c5e534b3cce4
AlexVipond <alex.r.vipond@gmail.com>
2021-07-09 23:23:57 -0500

    trim down theme in favor of JIT custom classes

--- src/index.js
+++ src/index.js
@@ -1,76 +1,39 @@
 import defaultTheme from 'tailwindcss/defaultTheme.js'
 import linearNumeric from '@baleada/tailwind-linear-numeric'
-import { fractions, withoutTailwindFractions, em, px, screen } from '@baleada/tailwind-theme-utils'
+import { screen } from '@baleada/tailwind-theme-utils'
 
 export default {
   ...linearNumeric(),
   colors: {
     inherit: 'inherit',
-    ...linearNumeric({ only: 'colors', increment: 10 }), // Tailwind 2.0.0 adding 50-level shades. Default linear numeric is bg-{color}-0.5 but I prefer bg-{color}-5 in most cases.
+    ...linearNumeric({ only: 'colors', increment: 10 }), // Tailwind 2.0.0 added 50-level shades. Default linear numeric is bg-{color}-0.5, but I prefer bg-{color}-5, so I use an increment of 10.
   },
   screens: {
     ...defaultTheme.screens,
     all: 0,
   },
-  spacing: {
-    ...linearNumeric({ only: 'spacing' }),
-    ...px({
-      '1': '1px',
-      '2': '2px',
-      '3': '4px',
-      '4': '6px',
-      '5': '8px',
-    }),
-    ...em({
-      '1/4': '0.25em',
-      '1/2': '.5em',
-      '1': '1em',
-      '5/4': '1.25em',
-      '3/2': '1.5em',
-    }),
-  },
-  height: theme => ({
-    ...withoutTailwindFractions(defaultTheme.height(theme)),
-    ...fractions({ unit: '%', set: 'baleada' }),
-    ...fractions({ unit: 'vh', set: 'baleada' }),
-  }),
   minWidth: theme => ({
     ...defaultTheme.minWidth,
     ...screen(theme('screens')),
-    ...fractions({ unit: '%', set: 'baleada' }),
-    ...fractions({ unit: 'vh', set: 'baleada' }),
   }),
   maxWidth: theme => ({
     ...linearNumeric({ only: 'maxWidth' }),
     ...screen(theme('screens')), // Already comes with screens but I'm including here to be explicit
-    ...fractions({ unit: '%', set: 'baleada' }),
-    ...fractions({ unit: 'vh', set: 'baleada' }),
   }),
   minHeight: theme => ({
     ...defaultTheme.minHeight,
     ...screen(theme('screens')), // Useful when styling landing pages to look nice in landscape orientation on mobile devices
-    ...fractions({ unit: '%', set: 'baleada' }),
-    ...fractions({ unit: 'vh', set: 'baleada' }),
   }),
   maxHeight: {
     ...defaultTheme.maxHeight,
-    ...fractions({ unit: '%', set: 'baleada' }),
-    ...fractions({ unit: 'vh', set: 'baleada' }),
   },
   width: theme => ({
-    ...withoutTailwindFractions(defaultTheme.width(theme)),
-    ...fractions({ unit: '%', set: 'baleada' }),
-    ...fractions({ unit: 'vw', set: 'baleada' }),
+    ...defaultTheme.width(theme),
     ...screen(theme('screens')),
   }),
   objectPosition: {
     ...defaultTheme.objectPosition,
     'center-top': 'center top',
     'center-bottom': 'center bottom',
-  },
-  opacity: {
-    ...defaultTheme.opacity,
-    '33': '.33',
-    '67': '.67',
   },
 }
--- tests/fixtures/expectedKeys.js
+++ tests/fixtures/expectedKeys.js
@@ -1,6 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme.js'
 import linearNumeric from '@baleada/tailwind-linear-numeric'
-import { fractions, em, px, screen } from '@baleada/tailwind-theme-utils'
+import { fractions, screen } from '@baleada/tailwind-theme-utils'
 
 // Functions used to generate keys here are all tested more explicitly in their individual packages
 
@@ -8,129 +8,50 @@ const baleadaScreens = {
         ...defaultTheme.screens,
         'all': 'stub',
       },
-      baleadaSpacing = [
-        ...Object.keys({
-          ...linearNumeric({ only: 'spacing' }),
-          ...px({
-            '1': 'stub',
-            '2': 'stub',
-            '3': 'stub',
-            '4': 'stub',
-            '5': 'stub',
-          }),
-          ...em({
-            '1/4': 'stub',
-            '1/2': 'stub',
-            '1': 'stub',
-            '5/4': 'stub',
-            '3/2': 'stub',
-          }),
-        })
-      ]
+      baleadaSpacing = Object.keys(linearNumeric({ only: 'spacing' }))
 
 export default {
-  colors: [
-    ...Object.keys(linearNumeric({ only: 'colors', increment: 10 }).blue),
-  ],
+  colors: Object.keys(linearNumeric({ only: 'colors', increment: 10 }).blue),
   spacing: [
     ...baleadaSpacing,
-  ],
-  height: [
-    ...baleadaSpacing,
-    'auto',
-    ...Object.keys({
-      ...fractions({ unit: '%', mode: 'baleada' }),
-      ...fractions({ unit: 'vh', mode: 'baleada' }),
-    })
   ],
   width: [
     ...baleadaSpacing,
     'auto',
     'min',
     'max',
+    'screen',
     ...Object.keys({
-      ...fractions({ unit: '%', mode: 'baleada' }),
-      ...fractions({ unit: 'vw', mode: 'baleada' }),
+      ...fractions({ unit: '%', set: 'tailwind' }),
       ...screen(baleadaScreens),
     }),
   ],
-  minWidth: [
-    ...Object.keys({
-      ...defaultTheme.minWidth,
-      ...screen(baleadaScreens), // Already comes with screens but I'm including here to be explicit
-      ...fractions({ unit: '%', mode: 'baleada' }),
-      ...fractions({ unit: 'vh', mode: 'baleada' }),
-    }),
-  ],
-  maxWidth: [
-    ...Object.keys({
-      ...linearNumeric({ only: 'maxWidth' }),
-      ...screen(baleadaScreens),
-      ...fractions({ unit: '%', mode: 'baleada' }),
-      ...fractions({ unit: 'vh', mode: 'baleada' }),
-    }),
-  ],
-  minHeight: [
-    ...Object.keys({
-      ...defaultTheme.minHeight,
-      ...screen(baleadaScreens),
-      ...fractions({ unit: '%', mode: 'baleada' }),
-      ...fractions({ unit: 'vh', mode: 'baleada' }),
-    }),
-  ],
-  maxHeight: [
-    ...Object.keys({
-      ...linearNumeric({ only: 'maxHeight' }),
-      ...fractions({ unit: '%', mode: 'baleada' }),
-      ...fractions({ unit: 'vh', mode: 'baleada' }),
-    }),
-  ],
-  blur: [
-    ...Object.keys(linearNumeric({ only: 'blur' })),
-  ],
-  borderRadius: [
-    ...Object.keys(linearNumeric({ only: 'borderRadius' })),
-  ],
-  borderWidth: [
-    ...Object.keys(linearNumeric({ only: 'borderWidth' })),
-  ],
-  boxShadow: [
-    ...Object.keys(linearNumeric({ only: 'boxShadow' })),
-  ],
-  dropShadow: [
-    ...Object.keys(linearNumeric({ only: 'dropShadow' })),
-  ],
-  flexGrow: [
-    ...Object.keys(linearNumeric({ only: 'flexGrow' })),
-  ],
-  flexShrink: [
-    ...Object.keys(linearNumeric({ only: 'flexShrink' })),
-  ],
-  fontSize: [
-    ...Object.keys(linearNumeric({ only: 'fontSize' })),
-  ],
-  fontWeight: [
-    ...Object.keys(linearNumeric({ only: 'fontWeight' })),
-  ],
-  letterSpacing: [
-    ...Object.keys(linearNumeric({ only: 'letterSpacing' })),
-  ],
-  lineHeight: [
-    ...Object.keys(linearNumeric({ only: 'lineHeight' })),
-  ],
-  strokeWidth: [
-    ...Object.keys(linearNumeric({ only: 'strokeWidth' })),
-  ],
-  ringOffsetWidth: [
-    ...Object.keys(linearNumeric({ only: 'ringOffsetWidth' })),
-  ],
-  ringWidth: [
-    ...Object.keys(linearNumeric({ only: 'ringWidth' })),
-  ],
-  transitionDuration: [
-    ...Object.keys(linearNumeric({ only: 'transitionDuration' })),
-  ],
-  transitionDelay: [
-    ...Object.keys(linearNumeric({ only: 'transitionDelay' })),
-  ],
+  minWidth: Object.keys({
+    ...defaultTheme.minWidth,
+    ...screen(baleadaScreens), // Already comes with screens but I'm including here to be explicit
+  }),
+  maxWidth: Object.keys({
+    ...linearNumeric({ only: 'maxWidth' }),
+    ...screen(baleadaScreens),
+  }),
+  minHeight: Object.keys({
+    ...defaultTheme.minHeight,
+    ...screen(baleadaScreens),
+  }),
+  blur: Object.keys(linearNumeric({ only: 'blur' })),
+  borderRadius: Object.keys(linearNumeric({ only: 'borderRadius' })),
+  borderWidth: Object.keys(linearNumeric({ only: 'borderWidth' })),
+  boxShadow: Object.keys(linearNumeric({ only: 'boxShadow' })),
+  dropShadow: Object.keys(linearNumeric({ only: 'dropShadow' })),
+  flexGrow: Object.keys(linearNumeric({ only: 'flexGrow' })),
+  flexShrink: Object.keys(linearNumeric({ only: 'flexShrink' })),
+  fontSize: Object.keys(linearNumeric({ only: 'fontSize' })),
+  fontWeight: Object.keys(linearNumeric({ only: 'fontWeight' })),
+  letterSpacing: Object.keys(linearNumeric({ only: 'letterSpacing' })),
+  lineHeight: Object.keys(linearNumeric({ only: 'lineHeight' })),
+  strokeWidth: Object.keys(linearNumeric({ only: 'strokeWidth' })),
+  ringOffsetWidth: Object.keys(linearNumeric({ only: 'ringOffsetWidth' })),
+  ringWidth: Object.keys(linearNumeric({ only: 'ringWidth' })),
+  transitionDuration: Object.keys(linearNumeric({ only: 'transitionDuration' })),
+  transitionDelay: Object.keys(linearNumeric({ only: 'transitionDelay' })),
 }
